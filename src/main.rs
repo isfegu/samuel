cad0553cddd04a6708bf7cfc89b019aecbb28cd5
isfegu <israelfenor@protonmail.com>
2023-02-18 11:33:43 +0100

    feat: crea módulo morse

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,12 @@
 use samuel::morse::translate;
+
 fn main() {
     let translated_string = translate("Hello 123456789 Samuel");
     println!("{}", translated_string);
 }
+
+// Instalar cargo-modules para poder visualizar cómodamente los módulos en un árbol.
+// cargo install cargo-modules
+// cargo modules generate tree
+// cargo modules generate tree --with-types
+// Módulos en Rust no están directamente relacionados con archivos
